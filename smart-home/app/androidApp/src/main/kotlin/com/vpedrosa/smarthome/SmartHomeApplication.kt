b90020e40c96ce53d03ff42adbee85c466a37f8a
Valentín Pedrosa <valentin@beebit.es>
2026-03-20 09:00:02 +0100

    fix #96: carga uiModule en SmartHomeApplication para resolver ViewModels
    SmartHomeApplication solo cargaba deviceModule y platformModule en Koin,
    omitiendo uiModule donde están registrados todos los ViewModels. Esto
    provocaba NoDefinitionFoundException al abrir cualquier pantalla.

--- smart-home/app/androidApp/src/main/kotlin/com/vpedrosa/smarthome/SmartHomeApplication.kt
+++ smart-home/app/androidApp/src/main/kotlin/com/vpedrosa/smarthome/SmartHomeApplication.kt
@@ -3,6 +3,7 @@ package com.vpedrosa.smarthome
 import android.app.Application
 import com.vpedrosa.smarthome.di.deviceModule
 import com.vpedrosa.smarthome.di.platformModule
+import com.vpedrosa.smarthome.ui.uiModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -11,7 +12,7 @@ class SmartHomeApplication : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@SmartHomeApplication)
-            modules(deviceModule, platformModule)
+            modules(platformModule, deviceModule, uiModule)
         }
     }
 }
